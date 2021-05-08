6a96a49dfc33f910af2664481b6e4034eabdadb8
Farhan <70105993+farfnd@users.noreply.github.com>
2021-05-08 22:14:05 +0800

    Update soal2b.c

--- soal2/soal2b.c
+++ soal2/soal2b.c
@@ -6,65 +6,62 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-#define baris 4
-#define kolom 6
+#define row 4
+#define column 6
 
 typedef long long ll;
-int A[baris][kolom], selisih, i, j, isZero = 0;
+int A[row][column], diff, i, j, isZero = 0;
 
-ll factorial(int n) {
-    if (n == 0)
-        return 1;
-    else
-        return n * factorial(n - 1);
+ll fact(int n) {
+    if (n == 0) return 1;
+    return n*fact(n-1);
 }
 
 ll diff_f(int n){
-	if (n == selisih) return 1;
-        else return n * diff_f(n - 1);
+	if (n == diff) return 1;
+        return n*diff_f(n-1);
 }
 
-void *kondisi(void* arg){
-	ll angka = *(ll*)arg;
-	if(isZero) printf("0 "); //0
-	else if(selisih<1) printf("%lld ", factorial(angka)); //<1
-	else printf("%lld ", diff_f(angka)); //>1
+void *print(void* argv){
+	ll n = *(ll*)argv;
+	if(isZero) printf("0 ");
+	else if(diff<1) printf("%lld ", fact(n)); 
+	else printf("%lld ", diff_f(n));
 }
 
 int main(){
 	key_t key = 1010;
-    int (*value)[kolom];
+    int (*hasil)[column];
     int shmid = shmget(key, sizeof(int[4][6]), IPC_CREAT | 0666);
-    value = shmat(shmid, NULL, 0);
+    hasil = shmat(shmid, NULL, 0);
 
     printf("Input matriks 4x6\n");
-	for (i=0; i<baris; i++) {
-    	for (j=0; j<kolom; j++) scanf("%d", &A[i][j]);
+	for (i=0; i<row; i++) {
+    	for (j=0; j<column; j++) scanf("%d", &A[i][j]);
   	}
 
-    pthread_t tid[baris*kolom];
+    pthread_t thread_id[row*column];
 
-    int index=0;
-    for(i = 0; i < baris; i++){
-        for(j = 0; j < kolom; j++){
+    int count=0;
+    for(i = 0; i < row; i++){
+        for(j = 0; j < column; j++){
         	isZero=0;
             ll *val = malloc(sizeof(ll[4][6]));
 
-            *val = value[i][j];
-            selisih = value[i][j] - A[i][j];
+            *val = hasil[i][j];
+            diff = hasil[i][j] - A[i][j];
 
-            if(value[i][j]==0 || A[i][j]==0) isZero=1;
+            if(hasil[i][j]==0 || A[i][j]==0) isZero=1;
 
-            pthread_create(&tid[index], NULL, &kondisi, val);
+            pthread_create(&thread_id[count], NULL, &print, val);
             sleep(1);
-            index++;
+            count++;
         }
         printf("\n");
     }
-    for (i = 0; i<index; i++)
-        pthread_join(tid[i], NULL);
+    for (i = 0; i<count; i++) pthread_join(thread_id[i], NULL);
 
-    shmdt(value);
+    shmdt(hasil);
     shmctl(shmid, IPC_RMID, NULL);
 
 	return 0;
