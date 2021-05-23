056eb2f3a69956a8aa858fd16b872c46c3042d62
Farhan <70105993+farfnd@users.noreply.github.com>
2021-05-23 18:01:53 +0800

    revisi soal 1 edit typo

--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -206,7 +206,7 @@ void delete_cmd(int client, char userdata[128]) {
 	    FILE *log = fopen("running.log", "a");
 	    fprintf(log, "Hapus : %s (%s)\n", filename, userdata);
 	    fclose(log)
-    } else return_client = send(client, "404", 100, 0);;
+    } else return_client = send(client, "404", 100, 0);
 }
 
 void see_cmd(int client) {
